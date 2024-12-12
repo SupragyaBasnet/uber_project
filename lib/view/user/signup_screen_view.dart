eb9ba2d118e52c54aaf1caa2b4c5cf554e83b386
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2024-12-12 18:41:06 +0545

    user_signup

--- lib/view/user/signup_screen_view.dart
+++ lib/view/user/signup_screen_view.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 import 'login_screen_view.dart'; // Import the login screen view
 import '../captain/captain_signup_screen_view.dart'; // Import captain signup screen view
 
-class SignupScreenView extends StatelessWidget {
+class SignupScreenView extends StatefulWidget {
   const SignupScreenView({super.key});
 
   @override
+  _SignupScreenViewState createState() => _SignupScreenViewState();
+}
+
+class _SignupScreenViewState extends State<SignupScreenView> {
+  // Controllers for input fields
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController phoneController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  // To toggle password visibility
+  bool _isPasswordHidden = true;
+
+  @override
   Widget build(BuildContext context) {
-    // MediaQuery to get screen width and height
+    // MediaQuery for dynamic sizing
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-
-    // TextEditingControllers to get the input text
-    TextEditingController nameController = TextEditingController();
-    TextEditingController phoneController = TextEditingController();
-    TextEditingController passwordController = TextEditingController();
 
     return Scaffold(
       appBar: AppBar(
@@ -29,72 +37,77 @@ class SignupScreenView extends StatelessWidget {
       backgroundColor: Colors.white,
       body: SafeArea(
         child: SingleChildScrollView(
-          padding: EdgeInsets.all(screenWidth * 0.05), // Dynamic padding based on screen size
+          padding: EdgeInsets.all(screenWidth * 0.05),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               // Name input field
               TextField(
                 controller: nameController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'What’s your Name',
                   border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 16),
-              
+
               // Phone number input field
               TextField(
                 controller: phoneController,
-                decoration: InputDecoration(
+                decoration: const InputDecoration(
                   labelText: 'What’s your Phone Number',
                   border: OutlineInputBorder(),
                 ),
               ),
               const SizedBox(height: 16),
-              
-              // Password input field
+
+              // Password input field with hide/unhide functionality
               TextField(
                 controller: passwordController,
                 decoration: InputDecoration(
                   labelText: 'What’s your Password',
-                  border: OutlineInputBorder(),
+                  border: const OutlineInputBorder(),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordHidden ? Icons.visibility_off : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordHidden = !_isPasswordHidden;
+                      });
+                    },
+                  ),
                 ),
-                obscureText: true,
+                obscureText: _isPasswordHidden,
               ),
               const SizedBox(height: 16),
-              
+
               // Create account button
               ElevatedButton(
                 onPressed: () {
-                  // Check if any field is empty
                   if (nameController.text.isEmpty ||
                       phoneController.text.isEmpty ||
                       passwordController.text.isEmpty) {
-                    // Show error message if any field is empty
                     ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: const Text('Please fill in all fields'),
-                        duration: const Duration(seconds: 2),
+                      const SnackBar(
+                        content: Text('Please fill in all fields'),
                         backgroundColor: Colors.red,
+                        duration: Duration(seconds: 2),
                       ),
                     );
                   } else {
-                    // Show success message if all fields are filled
                     ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: const Text('Account Created Successfully'),
-                        duration: const Duration(seconds: 2),
+                      const SnackBar(
+                        content: Text('Account Created Successfully'),
                         backgroundColor: Colors.green,
+                        duration: Duration(seconds: 2),
                       ),
                     );
-
-                    // After showing the message, navigate to the login screen
                     Future.delayed(const Duration(seconds: 2), () {
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => const LoginScreenView(), // Navigate to login screen after account creation
+                          builder: (context) => const LoginScreenView(),
                         ),
                       );
                     });
@@ -103,25 +116,24 @@ class SignupScreenView extends StatelessWidget {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.black,
                   foregroundColor: Colors.white,
-                  minimumSize: Size(double.infinity, screenHeight * 0.07), // Dynamic button height
+                  minimumSize: Size(double.infinity, screenHeight * 0.07),
                 ),
                 child: Text(
                   'Create account',
                   style: TextStyle(
-                    fontSize: screenWidth * 0.05, // Font size based on screen width
+                    fontSize: screenWidth * 0.05,
                   ),
                 ),
               ),
               const SizedBox(height: 16),
-              
+
               // Navigate to login screen
               TextButton(
                 onPressed: () {
-                  // Navigate to the login screen when already have an account
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const LoginScreenView(), // Navigate to login screen
+                      builder: (context) => const LoginScreenView(),
                     ),
                   );
                 },
@@ -129,16 +141,15 @@ class SignupScreenView extends StatelessWidget {
                   'Already have an account? Login here',
                   style: TextStyle(
                     color: Colors.blue,
-                    fontSize: screenWidth * 0.04, // Font size based on screen width
+                    fontSize: screenWidth * 0.04,
                   ),
                 ),
               ),
               const SizedBox(height: 16),
-              
+
               // Sign up as captain button
               ElevatedButton(
                 onPressed: () {
-                  // Navigate to the Captain Signup screen
                   Navigator.push(
                     context,
                     MaterialPageRoute(
@@ -149,12 +160,12 @@ class SignupScreenView extends StatelessWidget {
                 style: ElevatedButton.styleFrom(
                   backgroundColor: Colors.green,
                   foregroundColor: Colors.white,
-                  minimumSize: Size(double.infinity, screenHeight * 0.07), // Dynamic button height
+                  minimumSize: Size(double.infinity, screenHeight * 0.07),
                 ),
                 child: Text(
                   'Sign up as Captain',
                   style: TextStyle(
-                    fontSize: screenWidth * 0.05, // Font size based on screen width
+                    fontSize: screenWidth * 0.05,
                   ),
                 ),
               ),
