d1e0da24bb2508f3d7beda79eae0b656e66e0a8a
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2024-12-26 17:56:09 +0545

    login

--- lib/view/user/login_screen_view.dart
+++ lib/view/user/login_screen_view.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import '../captain/captain_login_screen_view.dart';
 import '../user/signup_screen_view.dart';
-import 'dashboard_screen_view.dart'; // Import the DashboardPage
+
+import 'dashboard/dashboard_user_view.dart';
 import 'forgot_password_view.dart'; // Import Forgot Password Page
 
 class LoginScreenView extends StatefulWidget {
