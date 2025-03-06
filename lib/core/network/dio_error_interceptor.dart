96da156ba624c3ac195b0916100234a4c9ecb51d
SupragyaBasnet <125958893+SupragyaBasnet@users.noreply.github.com>
2025-03-06 18:03:17 +0545

    added dio

--- lib/core/network/dio_error_interceptor.dart
+++ lib/core/network/dio_error_interceptor.dart
@@ -1,61 +1,32 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 
 class DioErrorInterceptor extends Interceptor {
   @override
   void onError(DioException err, ErrorInterceptorHandler handler) {
-    String errorMessage = _handleDioError(err);
-
-    // Log error to console
-    debugPrint("Dio Error: $errorMessage");
-
-    // Forward the error to be handled by the calling function
-    handler.reject(err);
-  }
-
-  String _handleDioError(DioException error) {
-    switch (error.type) {
-      case DioExceptionType.connectionTimeout:
-        return "Connection timed out. Please check your internet.";
-
-      case DioExceptionType.sendTimeout:
-        return "Request timed out. Please try again.";
-
-      case DioExceptionType.receiveTimeout:
-        return "Server took too long to respond.";
-
-      case DioExceptionType.badResponse:
-        return _handleHttpError(error.response);
-
-      case DioExceptionType.cancel:
-        return "Request was cancelled.";
-
-      case DioExceptionType.unknown:
-        return "An unexpected error occurred. Please try again.";
-
-      default:
-        return "Something went wrong. Please try again.";
+    if (err.response != null) {
+      if (err.response!.statusCode! >= 300) {
+        err = DioException(
+          requestOptions: err.requestOptions,
+          response: err.response,
+          error: err.response!.data['message'] ?? err.response!.statusMessage!,
+          type: err.type,
+        );
+      } else {
+        err = DioException(
+          requestOptions: err.requestOptions,
+          response: err.response,
+          error: 'Something went wrong!',
+          type: err.type,
+        );
+      }
+    } else {
+      // Handle Connection Errors
+      err = DioException(
+        requestOptions: err.requestOptions,
+        error: 'Connection Error!',
+        type: err.type,
+      );
     }
-  }
-
-  String _handleHttpError(Response? response) {
-    if (response == null) {
-      return "No response from server.";
-    }
-
-    switch (response.statusCode) {
-      case 400:
-        return "Bad request. Please check your input.";
-      case 401:
-        return "Unauthorized. Please log in again.";
-      case 403:
-        return "Access denied.";
-      case 404:
-        return "Requested resource not found.";
-      case 500:
-        return "Internal server error. Please try again later.";
-      default:
-        return "Server error: ${response.statusCode}.";
-    }
+    super.onError(err, handler);
   }
 }
